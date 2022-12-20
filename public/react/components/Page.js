4bed900dcc7b2b0f6394aa895224e92ede66b011
stanley <cjstanenemuo@yahoo.com>
2022-12-20 18:43:54 -0500

    completed

--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from "react";
+import apiURL from "../api";
 
 export const Page = (props) => {
+  const [page, setArticleData] = useState(undefined);
+  const selectArticles = async () => {
+    const res = await fetch(`${apiURL}/wiki/${props.page.slug}`);
+    const data = await res.json();
+    console.log(data);
+    //update state
+    setArticleData(data);
+  };
 
-  return <>
-    <h3>{props.page.title}</h3>
-  </>
-} 
-	
+  return (
+    <>
+      <h3 onClick={selectArticles}>{props.page.title}</h3>
+    </>
+  );
+};
